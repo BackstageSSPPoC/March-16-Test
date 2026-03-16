a9dc8e8edad0be1e11b4653ba97441ba4398565f
Chaitanya Shekhar Pandey <113713770+Chaitanya-Shekhar-Pandey@users.noreply.github.com>
2026-03-16 15:03:50 +0530

    Update welcome message in server response

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Welcome Chaitanya to Backstage SSP MVP!\n");
+  res.end("Welcome GSPANN TO Backstage SSP MVP!\n");
 });
 
 const PORT = 3000;
